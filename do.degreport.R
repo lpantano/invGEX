8b48075194cdd6eecdb0a378a64590db733fe85c
Lorena <lorena.pantano@gmail.com>
2014-09-05 09:26:32 -0400

    fixing coding style and get working degreport

--- do.degreport.R
+++ do.degreport.R
@@ -4,15 +4,18 @@ library(DEGreport)
 library(BiocParallel)
 
 
-do_degreport<-function(tags, dse, res, comp, path_out){
-    dir.create(path_out, showWarnings = FALSE)
-    design <- colData(dse)
-    c <- counts(dse, normalized =TRUE)
-    g1<-row.names(design[design$condition == comp[1],])
-    g2<-row.names(design[design$condition == comp[2],])
-    fc <- res[tags,"log2FoldChange"]
-    pvalue <- res$pvalue
-    createReport(g1, g2, c, tags, pvalue, fc, path = path_out, ncores = 2)
+do_degreport<-function(tags, dse, res, geno, path_out){
+    if (length(tags)>0){
+        dir.create(path_out, showWarnings = FALSE)
+        design <- colData(dse)
+        c <- counts(dse, normalized =TRUE)
+        g1<-row.names(design[design$condition == geno[1],])
+        g2<-row.names(design[design$condition == geno[2],])
+        fc <- res[tags,"log2FoldChange"]
+        pvalue <- res$pvalue
+        c <- c[row.names(res),]
+        createReport(g1, g2, c, tags, pvalue, fc, path = path_out, ncores = 2)
+        }
 }
 
 
@@ -31,8 +34,10 @@ do.dge.summary<-function(invname){
     print(invname)
     list.g<-vector()
     for (pop in population){
+        print(pop)
         for (i in 1:3) {
             gen<-paste0(comp[[i]],collapse="")
+            print(gen)
             dse.dir<-paste(sep="",path_res,invname,"/",type,"/",pop,".DESeq2",type,".parametric/",gen,"dse")
             rld.dir<-paste(sep="",path_res,invname,"/",type,"/",pop,".DESeq2",type,".parametric/",gen,"rld")
             if (file.exists(dse.dir)){
@@ -44,15 +49,15 @@ do.dge.summary<-function(invname){
                 }else{
                     res<-try(results(dse, independentFiltering=FALSE,cooksCutoff=FALSE),silent=TRUE)
                 }
-                if(class(res)=="DESeqResults"){
-                    res<-res[!is.na(res$padj),]
-                    res.all<-as.data.frame(mcols(dse,use.names=TRUE))
-                    if (sum(res$padj<=0.20,na.rm=T)>0){
-                        top<-as.data.frame(res[res$padj<=0.20,])
-                        filtered<-intersect(row.names(top),row.names(res.all))
-                        list.g<-unique(filtered)
-                        do_degreport(list.g, dse, res, comp[[i]],
-                                    paste0(path_res,invname,"/",type,"/",pop,".DESeq2",type,".parametric/",gen,"/"))
+                    if(class(res)=="DESeqResults"){
+                        res<-res[!is.na(res$padj),]
+                        res.all<-as.data.frame(mcols(dse,use.names=TRUE))
+                        if (sum(res$padj<=0.10,na.rm=T)>0){
+                            top<-as.data.frame(res[res$padj<=0.10,])
+                            filtered<-intersect(row.names(top),row.names(res.all))
+                            list.g<-unique(filtered)
+                            do_degreport(list.g, dse, res, comp[[i]],
+                                        paste0(path_res,invname,"/",type,"/",pop,".DESeq2",type,".parametric/",gen,"/"))
                     }
                 }
             }
