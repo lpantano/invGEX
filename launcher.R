09bd2628620c4c49d58ccab45450b08ce259a4b3
Lorena <lorena.pantano@gmail.com>
2015-03-26 11:22:29 -0400

    some comments to launcer

--- launcher.R
+++ launcher.R
@@ -4,11 +4,15 @@ dode<-function(x){
 	source("general.pipeline.function.R")
 	do.analysis(x)
 }
-load(all.inversions.obj)
+
+# I had an object with all names
+# load(all.inversions.obj)
 
 library(parallel)
 cl<-makeCluster(4) 
 clusterEvalQ(cl)
+# all.inversions is a character vector with the names of inversions
+# I recommend test first with only one inversion to check the pipeline works. 
 t<-system.time(clusterApplyLB(cl,all.inversions, dode))
 write.table(t,"/home/lpantano/time.process")
 stopCluster(cl)
